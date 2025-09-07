fc3a4f99dbea750782c082b600302e118d3dd7fc
Srdjan-P <srdjan.potrebic@gmail.com>
2025-09-07 23:13:59 +0200

    Filled out the forms with static values

--- src/components/FormAddFriend.js
+++ src/components/FormAddFriend.js
@@ -1,3 +1,15 @@
 export default function FormAddFriend() {
-  return <form className="form-add-friend">Form Add Friend</form>;
+  return (
+    <form className="form-add-friend">
+      <div className="space-between">
+        <label>👫 Friend name</label>
+        <input type="text" />
+      </div>
+      <div className="space-between">
+        <label>👨‍🏫 Image URL</label>
+        <input type="text" />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+  );
 }
--- src/components/FormSplitBill.js
+++ src/components/FormSplitBill.js
@@ -1,3 +1,31 @@
 export default function FormSplitBill() {
-  return <form className="form-split-bill">Form Split Bill</form>;
+  return (
+    <form className="form-split-bill">
+      <h2>SPLIT A BILL WITH X</h2>
+
+      <div className="space-between">
+        <label>💰 Bill Value</label>
+        <input type="text" />
+      </div>
+
+      <div className="space-between">
+        <label>💸 Your expense</label>
+        <input type="text" />
+      </div>
+
+      <div className="space-between">
+        <label>👫 X's expense</label>
+        <input type="text" disabled />
+      </div>
+
+      <div className="space-between">
+        <label>🤑 Who is paying the bill?</label>
+        <select>
+          <option value="user">You</option>
+          <option value="friend">X</option>
+        </select>
+      </div>
+      <button>Split Bill</button>
+    </form>
+  );
 }
